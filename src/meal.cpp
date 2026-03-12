b347023b6eb9c803d1c1d24698a8b422602050e2
Leonteva Kristina <kristinaleonteva30032007@gmail.com>
2026-03-12 14:12:32 +0300

    feat: mealcpp1

---
+++ src/meal.cpp
@@ -0,0 +1,15 @@
+﻿#include "meal.h"
+#include "exceptions.h"
+#include <numeric>
+
+Meal::Meal(std::string name)
+    : name(std::move(name))
+    , time(std::chrono::system_clock::now()) {}
+
+std::string Meal::getName() const noexcept {
+    return name;
+}
+
+std::chrono::system_clock::time_point Meal::getTime() const noexcept {
+    return time;
+}
