3fcdba6b107da63e186d97c6664729cb94285f91
Leonteva Kristina <kristinaleonteva30032007@gmail.com>
2026-03-13 14:29:53 +0300

    feat: add CalorieTracker constructor and getters

---
+++ src/calorie_tracker.cpp
@@ -0,0 +1,34 @@
+﻿#include "calorie_tracker.h"
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+
+CalorieTracker::CalorieTracker() 
+    : dataManager(std::make_unique<DataManager>("./data")) {}
+
+CalorieTracker& CalorieTracker::getInstance() {
+    static CalorieTracker instance;
+    return instance;
+}
+
+CalorieTracker::~CalorieTracker() {
+    try {
+        if (!currentUsername.empty()) saveData();
+    } catch (...) {}
+}
+
+UserProfile* CalorieTracker::getUserProfile() const noexcept {
+    return user.get();
+}
+
+const std::vector<std::unique_ptr<Meal>>& CalorieTracker::getMeals() const noexcept {
+    return meals;
+}
+
+const std::vector<std::shared_ptr<FoodItem>>& CalorieTracker::getAllFoods() const noexcept {
+    return foodDatabase;
+}
+
+std::string CalorieTracker::getCurrentUsername() const noexcept {
+    return currentUsername;
+}
